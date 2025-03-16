eff394fb8a03efe65add71ce9c73598b3af1b3b3
Rosa Richter <cosmic.lady.rosa@gmail.com>
2025-03-16 15:47:14 -0600

    Update ex_doc

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Scientist.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.34.2", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.37.3", only: :dev, runtime: false}
     ]
   end
 
